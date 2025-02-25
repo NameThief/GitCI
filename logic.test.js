bd125d8ca895c824ede561ef838a58c76f738f7b
adam <spotinediam@hotmail.fr>
2025-02-25 15:40:37 +0100

    crypto fix

--- logic.test.js
+++ logic.test.js
@@ -1,28 +1,31 @@
 const { addTodoAction, addTodo, deleteTodoAction, deleteTodo, completeTodoAction, completeTodo } = require("./logic");
 
 test("addTodo", () => {
-    const state = { todos: [] };
+    const state = { last_id:0, todos: [] };
     const action = addTodoAction("todo1");
     const newState = addTodo(state, action.payload.name);
     expect(newState).toEqual({
-      todos: [{ id: expect.any(String), name: "todo1", completed: false }],
+      last_id:1,
+      todos: [{ id: 0, name: "todo1", completed: false }],
     });
   });
 
 test("deleteTodo", () => {
-    const state = { todos: [{ id: 1, name: "todo1", completed: false }] };
+    const state = {last_id:3, todos: [{ id: 1, name: "todo1", completed: false }] };
     const action = deleteTodoAction(1);
-    const newState = deleteTodo(state, action.payload.id);
+    const newState = deleteTodo(state, 1);
     expect(newState).toEqual({
+      last_id:3,
       todos: [],
     });
   });
 
 test("completeTodo", () => {
-    const state = { todos: [{ id: 1, name: "todo1", completed: false }] };
+    const state = {last_id:3, todos: [{ id: 1, name: "todo1", completed: false }] };
     const action = completeTodoAction(1);
-    const newState = completeTodo(state, action.payload.id);
+    const newState = completeTodo(state, 1);
     expect(newState).toEqual({
+      last_id:3,
       todos: [{ id: 1, name: "todo1", completed: true }],
     });
   });
